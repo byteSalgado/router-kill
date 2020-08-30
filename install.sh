2e82383579f3eea4ccc9d7b18737b3cff772a8ef
Facu Salgado <63315825+byteSalgado@users.noreply.github.com>
2020-08-30 02:00:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+#Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
 clear
 
 if [[ $EUID -ne 0 ]]; then	
